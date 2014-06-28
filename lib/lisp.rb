777185ca722fefbbba6b472b3a97ec2396a3d779
Kajetan Bojko <kai@shutl.co.uk>
2014-06-28 17:20:10 +0100

    eval finished

--- lib/lisp.rb
+++ lib/lisp.rb
@@ -46,14 +46,32 @@ def evaluate(x, env = @@global_env)
     env.find(x)[x]
   elsif !x.is_a?(Array)
     x
-  elsif x.first == 'quote'
+  elsif x.first == :quote
     x.last
-  elsif x.first == 'if'
+  elsif x.first == :if
     _, test, conseq, alt = x
     branch = evaluate(test, env) ? conseq : alt
     evaluate(branch, env)
-  elsif x.first == 'set!'
+  elsif x.first == :set!
     _, name, exp = x
     env.find(name)[name] = evaluate(exp, env)
+  elsif x.first == :define
+    _, name, exp = x
+    env[name] = evaluate(exp, env)
+  elsif x.first == :lambda
+    _, names, exp = x
+    lambda do |*vals|
+      evaluate(exp, Env.new(names, vals, env)) 
+    end
+  elsif x.first == :begin
+    val = nil
+    x[1..-1].each do |exp|
+      val = evaluate(exp, env)
+    end
+    val
+  else # procedure call
+    exps = x.map {|exp| evaluate(exp, env)}
+    proc = exps[0]
+    proc.call(*exps[1..-1])
   end
 end
--- spec/functional_spec.rb
+++ spec/functional_spec.rb
@@ -7,8 +7,8 @@ describe 'lisp' do
   end
 
   it 'reads tokens' do
-    tree = read(['(', 'a', '(', '+', '3.2', '0', ')', ')'].reverse)
-    expect(tree).to eq([:a, [:'+', 3.2, 0]])
+    tree = read(['(', 'define', '(', '+', '3.2', '0', ')', ')'].reverse)
+    expect(tree).to eq([:define, [:'+', 3.2, 0]])
   end
 
   it 'evals variable' do
@@ -21,25 +21,53 @@ describe 'lisp' do
   end
 
   it 'evals quote' do
-    exp = ['quote', [:a, 2, 3]]
+    exp = [:quote, [:a, 2, 3]]
     expect(evaluate(exp)).to eq([:a, 2, 3])
   end
 
   it 'evals if then' do
-    exp = ['if', true, 1, 2]
+    exp = [:if, true, 1, 2]
     expect(evaluate(exp)).to eq(1)
   end
 
   it 'evals if else' do
-    exp = ['if', false, 1, 2]
+    exp = [:if, false, 1, 2]
     expect(evaluate(exp)).to eq(2)
   end
 
   it 'evals set!' do
     env = Env.new([:a], [0])
-    exp = ['set!', :a, 1]
+    exp = [:set!, :a, 1]
     evaluate(exp, env)
 
     expect(evaluate(:a, env)).to eq(1)
+  end
+
+  it 'evals define' do
+    env = Env.new([], [])
+    exp = [:define, :a, 1]
+    evaluate(exp, env)
+
+    expect(evaluate(:a, env)).to eq(1)
+  end
+
+  it 'evals lambda' do
+    env = Env.new([], [])
+    exp = [:lambda, [:x], :x]
+    lbd = evaluate(exp, env)
+
+    expect(lbd.call(3)).to eq(3)
+  end
+
+  it 'evals sequence' do
+    env = Env.new([], [])
+    exp = [:begin, [:define, :x, 1], [:set!, :x, 2], :x]
+    expect(evaluate(exp, env)).to eq(2)
+  end
+
+  it 'evals procedure calls' do
+    env = Env.new([], [])
+    exp = [[:lambda, [:x], :x], 3]
+    expect(evaluate(exp, env)).to eq(3)
   end
 end
