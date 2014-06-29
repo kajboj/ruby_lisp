67cf0a6ef4e63a9fd3f4c4630dfb141c523219d1
Kajetan Bojko <kai@shutl.co.uk>
2014-06-29 11:31:24 +0100

    building encoding tree

--- huffman.lisp
+++ huffman.lisp
@@ -2,13 +2,16 @@
   (define null (list))
 
   (define leaf 
-    (lambda (char weight) (list char weight null null)))
+    (lambda (char weight) (list (list char) weight null null)))
 
   (define fork 
     (lambda (chars weight left right) (list chars weight left right)))
 
   (define weight
     (lambda (tree) (car (cdr tree))))
+
+  (define char
+    (lambda (leaf) (car (chars leaf))))
 
   (define chars
     (lambda (tree) (car tree)))
@@ -38,29 +41,49 @@
       (cons t null)
       (if (empty? t)
        (pack (cdr s) (leaf (car s) 1))
-       (if (eql? (car s) (chars t))
-        (pack (cdr s) (leaf (chars t) (+ (weight t) 1)))
+       (if (eql? (car s) (char t))
+        (pack (cdr s) (leaf (char t) (+ (weight t) 1)))
         (cons t (pack s null)))))))
 
   (define tree-cmp 
     (lambda (t1 t2)
       (< (weight t1) (weight t2))))
 
-  (define msg (quote (h e l l o _ w o r l d)))
-  (define sorted-msg (sort msg null <))
-  (define packed (pack sorted-msg null))
-  (define sorted-leaves (sort packed null tree-cmp))
-
   (define cadr (lambda (list) (car (cdr list))))
   (define cddr (lambda (list) (cdr (cdr list))))
 
-)
+  (define singleton? 
+    (lambda (xs) (eql? (length xs) 1)))
+
+  (define combine-chars
+    (lambda (t1 t2)
+      (append (chars t1) (chars t2))))
+
+  (define combine-weights
+    (lambda (t1 t2)
+      (+ (weight t1) (weight t2))))
+
+  (define combine
+    (lambda (t1 t2) 
+      (fork
+        (combine-chars t1 t2)
+        (combine-weights t1 t2)
+        t1
+        t2)))
 
   (define build-tree 
     (lambda (trees)
-      (if (eql? (length trees) 1)
+      (if (singleton? trees)
         (car trees)
         (build-tree
           (merge 
             (combine (car trees) (cadr trees))
-            (cddr trees))))))
+            (cddr trees)
+            tree-cmp)))))
+
+  (define msg (quote (h e l l o _ w o r l d)))
+  (define sorted-msg (sort msg null <))
+  (define packed (pack sorted-msg null))
+  (define sorted-leaves (sort packed null tree-cmp))
+  (define coding-tree (build-tree sorted-leaves))
+)
